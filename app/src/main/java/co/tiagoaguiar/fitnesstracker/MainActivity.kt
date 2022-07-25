92cecfa122ef74204a41d54f6d9c2d3ee08e1830
ERBCosta <eltonr@gmail.com>
2022-07-25 15:51:34 -0300

    Criado o evento de touch para mudar da tela principal para a tela imc activity

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,30 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    //declaramos aqui o botão que escutaremos o evento de touch criando o botão:
+    private lateinit var btnImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //abaixo é dado um valor para a variável btn para ela não ficar nula
+        btnImc = findViewById(R.id.btn_imc)
+
+        //para escutar os eventos de touch precisa do SetOnClickListener
+        btnImc.setOnClickListener {
+            //Para navegar para a proxima tela sempre precisaremos de um intent
+            //primeiro passo é declarar uma variável nesse caso chamada de i e construimos o código
+            //padrão da linha abaixo
+            val i = Intent(this,ImcActivity::class.java)
+            //agora que criamos a inteção na linha de cima, é necessário inicializar a intenção
+            //com mais uma função padrão: StartActivity
+            startActivity(i)
+        }
     }
 }
