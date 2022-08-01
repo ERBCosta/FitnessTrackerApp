c077944a7e9c9bc47031738faa84fe018a3e857f
ERBCosta <eltonr@gmail.com>
2022-08-01 16:23:38 -0300

    Alterações na parte logica: Feito código para validar os campos de entrada peso e altura não podem ser vazios e nem podem começar com zero ou ter peso e altura 0.

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -2,10 +2,45 @@ package co.tiagoaguiar.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if(!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            //aqui sucesso
+        }
+
+    }
+
+    private fun validate(): Boolean {
+        //não pode inserir valores nulos/vazio
+        //não pode começar com número 0
+
+        //true && true   = true
+        //true && false  = false
+        //false && true  = false
+        //false && false = false
+
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
     }
 }
